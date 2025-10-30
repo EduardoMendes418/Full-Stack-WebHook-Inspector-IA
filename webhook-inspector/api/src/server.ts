a5044dfc92b8481e69e52fe9b80e1a1669d98cb8
eduardo <eduardomendes418@gmail.com>
2025-10-30 11:38:13 -0300

    [FSW-41] fix - api server

--- webhook-inspector/api/src/server.ts
+++ webhook-inspector/api/src/server.ts
@@ -1,15 +1,14 @@
-import { fastify } from "fastify";
-import {
-  serializerCompiler,
-  validatorCompiler,
-  jsonSchemaTransform,
-  type ZodTypeProvider,
-} from "fastify-type-provider-zod";
-import { fastifyCors } from "@fastify/cors";
-import { fastifySwagger } from "@fastify/swagger";
-import ScalarApiReference from "@scalar/fastify-api-reference";
-import { listWebhooks } from "./routes/list-webhooks";
-import { env } from './env'
+import { fastify } from 'fastify'
+import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { serializerCompiler, validatorCompiler, jsonSchemaTransform, type ZodTypeProvider } from 'fastify-type-provider-zod'
+import ScalarApiReference from '@scalar/fastify-api-reference'
+
+import { env } from './config/env'
+import { listWebhooks } from './routes/list-webhooks'
+import { getWebhook } from './routes/get-webhook'
+import { deleteWebhook } from './routes/delete-webhook'
+import { captureWebhook } from './routes/capture-webhook'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
@@ -19,7 +18,7 @@ app.setSerializerCompiler(serializerCompiler)
 app.register(fastifyCors, {
   origin: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  // credentials: true,
+  credentials: true
 })
 
 app.register(fastifySwagger, {
@@ -28,19 +27,21 @@ app.register(fastifySwagger, {
       title: 'Webhook Inspector API',
       description: 'API for capturing and inspecting webhook requests',
       version: '1.0.0',
-    },
+    }
   },
-  transform: jsonSchemaTransform,
+  transform: jsonSchemaTransform
 })
 
 app.register(ScalarApiReference, {
-  routePrefix: '/docs',
+  routePrefix: '/docs'
 })
 
+app.register(getWebhook)
 app.register(listWebhooks)
+app.register(deleteWebhook)
+app.register(captureWebhook)
 
-
-app.listen({ port: env.PORT , host: "0.0.0.0" }).then(() => {
-  console.log('🔥 HTTP server running on http://localhost:3333!')
-  console.log('📚 Docs available at http://localhost:3333/docs')
-});
+app.listen({ port: env.PORT, host: '0.0.0.0' }).then(() => {
+  console.log(`HTTP server running on http://localhost:${env.PORT}!`)
+  console.log('Docs available at http://localhost:3333/docs')
+})
