2dd5e52ada57de72cdfea003a3d0da6036df4fa2
eduardo <eduardomendes418@gmail.com>
2025-10-30 08:57:49 -0300

    [FSW-33] fix - layout

--- webhook-inspector/web/src/routes/__root.tsx
+++ webhook-inspector/web/src/routes/__root.tsx
@@ -1,7 +1,7 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { PanelGroup, Panel, PanelResizeHandle } from 'react-resizable-panels'
-//import { Sidebar } from '../components/sidebar'
+import { Sidebar } from '../components/sidebar'
 
 const queryClient = new QueryClient()
 
@@ -10,7 +10,7 @@ const RootLayout = () => (
     <div className="h-screen bg-zinc-900">
       <PanelGroup direction="horizontal">
         <Panel defaultSize={20} minSize={15} maxSize={40}>
-          {/*<Sidebar />*/}
+          <Sidebar />
         </Panel>
 
         <PanelResizeHandle className="w-px bg-zinc-700 hover:bg-zinc-600 transition-colors duration-150" />
